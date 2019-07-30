f5c0909d5a34edb11b34ade44c5bb6c6f4ea4557
Andrew <Jensen1.Andrew@gmail.com>
2019-07-30 08:32:09 -0400

    first commit movie list

---
+++ Lab11 Movie List/Movies.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11_Movie_List
+{
+    class Movies
+    {
+        //fields
+        private string _title;
+        private string _category;
+
+        public static List<Movies> animatedMovies = new List<Movies>
+        {
+            new Movies("Aladdin", "Animated"),
+            new Movies("Shrek", "Animated")
+        };
+        
+        public static List<Movies> dramaMovies = new List<Movies>
+        {
+            new Movies("Schindler's List", "Drama"),
+            new Movies("Good Will Hunting", "Drama")
+        };
+        public static List<Movies> horrorMovies = new List<Movies>
+        {
+            new Movies("Friday the 13th", "Horror"),
+            new Movies("Us", "Horror"),
+            new Movies("IT", "Horror")
+        };
+        public static List<Movies> scifiMovies = new List<Movies>
+        {
+             new Movies("Avatar", "Scifi"),
+             new Movies("Star Wars A New Hope", "Scifi"),
+             new Movies("The Martian", "Scifi")
+        };
+
+        public string Movie
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+        public string Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+            }
+        }
+       
+        public Movies()
+        {
+
+        }
+        public Movies(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+        
+        public static void DisplayMovieList()
+        {
+            Console.WriteLine("1. List All Movies");
+            Console.WriteLine("2. List Movies By Genre");
+            Console.WriteLine("3. Exit");
+
+        }
+        public static void DisplayAllMovies()
+        {
+            foreach (Movies movie in animatedMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+            foreach (Movies movie in dramaMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+            foreach (Movies movie in horrorMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+            foreach (Movies movie in scifiMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+        }
+        public static void DisplayMovieByGenre()
+        {
+            bool go = true;
+            while (go)
+            {
+                Console.WriteLine("What Genre would you like to view?\n");
+                Console.WriteLine("1: Animated, 2: Drama, 3: Horror, 4: Scifi, 5: Return to Main Menu");
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    Console.WriteLine();
+                    if (result == 1)
+                    {
+                        DisplayAnimatedMovies();
+                    }
+                    else if (result == 2)
+                    {
+                        DisplayDramaMovies();
+                    }
+                    else if (result == 3)
+                    {
+                        DisplayHorrorMovies();
+                    }
+                    else if (result == 4)
+                    {
+                        DisplayScifiMovies();
+                    }
+                    else if (result == 5)
+                    {
+                        go = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That is not a vaild input please try again.");
+                    go = false;
+                }
+            }
+        }
+        public static void DisplayAnimatedMovies()
+        {
+            foreach (Movies movie in animatedMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+        }
+        public static void DisplayDramaMovies()
+        {
+            foreach (Movies movie in dramaMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+        }
+        public static void DisplayHorrorMovies()
+        {
+            foreach (Movies movie in horrorMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+        }
+        public static void DisplayScifiMovies()
+        {
+            foreach (Movies movie in scifiMovies)
+            {
+                Console.WriteLine($"{movie._title}");
+            }
+        }
+            
+    }
+}
---
+++ Lab11 Movie List/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool go = true;
+            while (go)
+            {
+                Console.WriteLine("Welcome to the Movie List please see options below\n");
+                Movies.DisplayMovieList();
+                Console.WriteLine();
+                Console.WriteLine("What would you like to see? please select a number to view.\n");
+
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result == 1)
+                    {
+                        Movies.DisplayAllMovies();
+                    }
+                    else if (result == 2)
+                    {
+                        Movies.DisplayMovieByGenre();
+                        Movies.DisplayMovieList();
+                    }
+                    else if (result == 3)
+                    {
+                        go = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry that is not a vaild option please try again.");
+                }
+            }
+        }
+    }
+}
